651ebddb7c9998ab9ea0b50f6173a32cd080233a
bruxelline <l.arnolin@gmail.com>
2014-10-07 16:50:14 +0200

    fin du cours

---
+++ FirstMVC2/Models/Reponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC2.Models
+{
+    public class Reponse
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string telephone { get; set; }
+        public bool? presence { get; set; }
+    }
+}
